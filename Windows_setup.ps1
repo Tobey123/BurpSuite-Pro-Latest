c024eb7feaee96f0ab3a502e76b0c5004804fad2
X!v3r <vanniealdamar@gmail.com>
2023-07-08 11:25:22 +0800

    Update Windows_setup.ps1

--- Windows_setup.ps1
+++ Windows_setup.ps1
@@ -1,17 +1,3 @@
-# Name is Important
-write-host "  .d8888b.           888       .d8888b.               8888888888P                  888    " -foregroundcolor "Red"
-write-host " d88P  Y88b          888      d88P  Y88b                    d88P                   888    " -foregroundcolor "Red"
-write-host " 888    888          888           .d88P                   d88P                    888    " -foregroundcolor "Red"
-write-host " 888        888  888 88888b.      8888'  888d888          d88P    .d88b.  .d8888b  888888 " -foregroundcolor "Red"
-write-host " 888        888  888 888 '88b      'Y8b. 888P'           d88P    d8P  Y8b 88K      888    " -foregroundcolor "white"
-write-host " 888    888 888  888 888  888 888    888 888            d88P     88888888 'Y8888b. 888    " -foregroundcolor "white"
-write-host " Y88b  d88P Y88b 888 888 d88P Y88b  d88P 888           d88P      Y8b.          X88 Y88b.  " -foregroundcolor "white"
-write-host "  'Y8888P'   'Y88888 88888P'   'Y8888P'  888          d8888888888 'Y8888   88888P'  'Y888 " -foregroundcolor "green"
-write-host "                 888                                                                      " -foregroundcolor "green"
-write-host "            Y8b d88P                                                                      " -foregroundcolor "green"
-write-host "             'Y88P'                                                                       " -foregroundcolor "green"
-
-echo "  This script is made by Cyber Zest"
 # Set Wget Progress to Silent, Becuase it slows down Downloading by 50x
 echo "Setting Wget Progress to Silent, Becuase it slows down Downloading by 50x`n"
 $ProgressPreference = 'SilentlyContinue'
